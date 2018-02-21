35a5c15ee387843af3fabb4f99d1bb60bc30ac87
Christopher Fu <cfu6@binghamton.edu>
2018-02-21 00:37:55 -0500

    initial commit, stemmer completed

---
+++ hw2.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import argparse
+import os
+import re
+from nltk.stem.porter import * 
+
+def getArgs():
+    p = argparse.ArgumentParser(description='Read and proceess a series of txt files in a directory to one clean stemmed text file.')
+    p.add_argument('stemDir', help='directory of files to stem')
+    p.add_argument('outFile', help='dir to output txt of stemmed files')
+    return p.parse_args()
+
+#main function
+if __name__ == "__main__":
+    args = getArgs()
+    stemDir  = args.stemDir
+    outFile = args.outFile
+
+    regex = re.compile('[^a-zA-Z0-9]')
+    #list of words in all txt documents in folder
+    unstemmed_words_list = []
+    #for current file in dir
+    for filename in os.listdir(stemDir):
+        if filename.endswith(".txt"):
+            filedir = stemDir+'/'+filename
+            #print(filedir)
+            with open(filedir,'r',encoding='utf-8', errors='ignore') as openFile:
+                for line in openFile:
+                    for word in line.split():
+                        cleaned = regex.sub('', word)
+                        if len(cleaned) > 0:
+                            unstemmed_words_list.append(cleaned.lower())
+    #print(unstemmed_words_list)
+    stemmer = PorterStemmer()
+    stemmed_words_list = [stemmer.stem(plural) for plural in unstemmed_words_list]
+    #print(stemmed_words_list)
+
+    with open(outFile, 'w') as stemmed_file:
+        for word in stemmed_words_list:
+            stemmed_file.write(word + " ")
