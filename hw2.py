a4a205e54d34939905c653fdfab949c62a196471
Christopher Fu <cfu6@binghamton.edu>
2018-02-22 01:29:58 -0500

    framework for NB done

--- hw2.py
+++ hw2.py
@@ -1,41 +1,71 @@
-import pandas as pd
 import numpy as np
-import argparse
-import os
-import re
-from nltk.stem.porter import * 
+import argparse, os, re, functools
+from stemmer import stemDoc
+
+#Naive Bayes Classifier
 
 def getArgs():
     p = argparse.ArgumentParser(description='Read and proceess a series of txt files in a directory to one clean stemmed text file.')
-    p.add_argument('stemDir', help='directory of files to stem')
-    p.add_argument('outFile', help='dir to output txt of stemmed files')
+    p.add_argument('ham', help='spam txt file')
+    p.add_argument('spam', help='ham txt file')
+    p.add_argument('hamDir', help='needed to count number of ham files')
+    p.add_argument('spamDir', help='needed to count number of spam files')
     return p.parse_args()
+
+def initBag(ham):
+    d = {}
+    with open(ham,'r') as openFile:
+        for line in openFile:
+            for word in line.split():
+                if not word in d:
+                    d[word] = 1
+                else:
+                    d[word] += 1
+    return d
+
+def chooseClass():
+    pass
+
+def calcCondProb(prior, probList):
+    logList = map((lambda x: np.log(x)), probList)
+    prodRes = functools.reduce((lambda x,y: x + y), logList)
+    return prodRes*prior
+
+def genProbFromEmail(emailFile, hamBag, spamBag, HorS="HAM"):
+    #from email, take each word, calculate prob, add to list
+    percList = []
+    with open(emailFile, 'r') as openFile:
+        for line in openFile:
+            for word in line.split():
+                #calc probability from word
+                print(word)
+                hInst=hamBag.get(word, 0)
+                sInst=spamBag.get(word, 0)
+                if hInst == 0 or sInst == 0:
+                    percList.append(0)
+                    print('0')
+                else: 
+                    frac = (hInst/(hInst+sInst)) if HorS == "HAM" else (sInst/(hInst+sInst))
+                    percList.append(frac)
+                    print(frac)
+    return percList
 
 #main function
 if __name__ == "__main__":
     args = getArgs()
-    stemDir  = args.stemDir
-    outFile = args.outFile
+    ham = args.ham
+    spam = args.spam
+    hamDir = args.hamDir
+    spamDir = args.spamDir
 
-    regex = re.compile('[^a-zA-Z0-9]')
-    #list of words in all txt documents in folder
-    unstemmed_words_list = []
-    #for current file in dir
-    for filename in os.listdir(stemDir):
-        if filename.endswith(".txt"):
-            filedir = stemDir+'/'+filename
-            #print(filedir)
-            with open(filedir,'r',encoding='utf-8', errors='ignore') as openFile:
-                for line in openFile:
-                    for word in line.split():
-                        cleaned = regex.sub('', word)
-                        if len(cleaned) > 0:
-                            unstemmed_words_list.append(cleaned.lower())
-    #print(unstemmed_words_list)
-    stemmer = PorterStemmer()
-    stemmed_words_list = [stemmer.stem(plural) for plural in unstemmed_words_list]
-    #print(stemmed_words_list)
-
-    with open(outFile, 'w') as stemmed_file:
-        for word in stemmed_words_list:
-            stemmed_file.write(word + " ")
+    hamBag = initBag(ham)
+    spamBag = initBag(spam)
+    hamCount = len(os.listdir(hamDir))    
+    spamCount = len(os.listdir(spamDir))    
+    prior_ham = hamCount / (hamCount+spamCount)
+    prior_spam = spamCount / (hamCount+spamCount)
+    
+    l = genProbFromEmail("minitest_ham_stemmed.txt",hamBag,spamBag,"HAM");
+    l2 = genProbFromEmail("minitest_ham_stemmed.txt",hamBag,spamBag,"SPAM");
+    #print(calcCondProb(1,[.5,.5,.5]))
+    #print(np.exp(calcCondProb(1,[.5,.5,.5])))
--- stemmer.py
+++ stemmer.py
@@ -1,8 +1,5 @@
-import pandas as pd
 import numpy as np
-import argparse
-import os
-import re
+import argparse, os, re
 from nltk.stem.porter import * 
 
 def getArgs():
@@ -10,6 +7,20 @@ def getArgs():
     p.add_argument('stemDir', help='directory of files to stem')
     p.add_argument('outFile', help='dir to output txt of stemmed files')
     return p.parse_args()
+
+def stemDoc(docDir):
+    unstemmed_words_list = []
+    regex = re.compile('[^a-zA-Z0-9]')
+    with open(filedir,'r',encoding='utf-8', errors='ignore') as openFile:
+        for line in openFile:
+            for word in line.split():
+                cleaned = regex.sub('', word)
+                if len(cleaned) > 0:
+                    unstemmed_words_list.append(cleaned.lower())
+    #print(unstemmed_words_list)
+    stemmer = PorterStemmer()
+    stemmed_words_list = [stemmer.stem(plural) for plural in unstemmed_words_list]
+    return stemmed_words_list
 
 #main function
 if __name__ == "__main__":
