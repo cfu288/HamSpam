628596cc1d7bde01814f0a251b8d77d68357438a
Christopher Fu <cfu6@binghamton.edu>
2018-02-24 00:53:04 -0500

    Fixed NB, added laplace, spent 3 hours debugging a multiplication error i missed because i can't do basic arithmitic

--- hw2.py
+++ hw2.py
@@ -4,7 +4,6 @@ from stemmer import stemDir
 from stemmer import stemDoc
 
 #Naive Bayes Classifier
-
 def getArgs():
     p = argparse.ArgumentParser(description='Read and proceess a series of txt files in a directory to one clean stemmed text file.')
     p.add_argument('ham', help='spam txt file')
@@ -28,69 +27,62 @@ def chooseClass():
     pass
 
 def calcCondProb(prior, probList):
-    #print(probList)
-    logList = map((lambda x: 0 if x ==0 else np.log(x)), probList)
-    prodRes = functools.reduce((lambda x,y: x + y), logList)
-    return prodRes*prior
+    probList.append(prior)
+    logList = map((lambda x: np.log(x)), probList)
+    #print("loglist: {}".format(logList))
+    prodRes = sum(logList)
+    return prodRes
 
-def genProbFromEmail(emailFile, hamBag, spamBag, HorS="HAM"):
+def genProbFromList(list, hamBag, spamBag, HorS):
     #from email, take each word, calculate prob, add to list
     percList = []
-    with open(emailFile, 'r') as openFile:
-        for line in openFile:
-            for word in line.split():
-                #calc probability from word
-                print(word,end=': ')
-                hInst=hamBag.get(word, 0)
-                sInst=spamBag.get(word, 0)
-                if hInst == 0 or sInst == 0:
-                    percList.append(0)
-                    print('0')
-                else: 
-                    frac = (hInst/(hInst+sInst)) if HorS == "HAM" else (sInst/(hInst+sInst))
-                    percList.append(frac)
-                    print(frac)
-    return percList
-
-def genProbFromList(list, hamBag, spamBag, HorS="HAM"):
-    #from email, take each word, calculate prob, add to list
-    percList = []
+    totNumWordsInHam = sum(hamBag.values())
+    totUniqueWordsInHam = len(hamBag)
+    totNumWordsInSpam = sum(spamBag.values())
+    totUniqueWordsInSpam = len(spamBag)
     for word in list:
         #calc probability from word
-        #print(word,end=': ')
         hInst=hamBag.get(word, 0)
         sInst=spamBag.get(word, 0)
-        if hInst == 0 or sInst == 0:
-            #implement laplace transform
-            percList.append(0)
-            #print('0')
-        else: 
-            frac = (hInst/(hInst+sInst)) if HorS == "HAM" else (sInst/(hInst+sInst))
+        #Laplace smoothing of 1
+        if (hInst == 0 and HorS == "HAM") or (sInst == 0 and HorS == "SPAM"):
+            #P(word|ham)laplace = 1 / totNumWordsinHam + totalUniqeWordInHam
+            frac = 1/(totNumWordsInHam + totUniqueWordsInHam) if HorS == "HAM" else 1/(totNumWordsInSpam + totUniqueWordsInSpam) 
             percList.append(frac)
-            #print(frac)
+            #pr = "NOTFOUND {}/{},{}".format(hInst, totNumWordsInHam + totUniqueWordsInHam,frac) if HorS == "HAM" else "NOTFOUND {}/{}, {}".format(sInst, totNumWordsInSpam + totUniqueWordsInSpam,frac)
+        else: 
+            #P(word|ham)laplace = #ofWordInHam + 1 / totNumWordsinHam + totalUniqeWordInHam
+            frac = (hInst+1)/(totNumWordsInHam + totUniqueWordsInHam) if HorS == "HAM" else (sInst+1)/(totNumWordsInSpam + totUniqueWordsInSpam) 
+            percList.append(frac)
+            #pr = "   FOUND {}/{}, {}".format(hInst, totNumWordsInHam + totUniqueWordsInHam, frac) if HorS == "HAM" else "   FOUND {}/{}, {}".format(sInst, totNumWordsInSpam + totUniqueWordsInSpam, frac)
     return percList
 
 def test(dir,hamBag, spamBag, HorS, hprior, sprior):
-    #for each email in dir
-    # run test 
-    #if accurate add to counter, and total count
-    # if not just increase total count
-    # return percent correct
     correct = 0
     total = 0
+    #for each email in dir
     for doc in os.listdir(dir):
         total += 1
         #print("stemming {}\{}".format(dir,doc))
         l = stemDoc(dir+doc)
+        #print("CALC HAM PROB ON TEST EMAIL")
         l2 = genProbFromList(l, hamBag, spamBag, "HAM")
+        #print("CALC SPAM PROB ON TEST EMAIL")
         l3 = genProbFromList(l, hamBag, spamBag, "SPAM")
+        #print("hprior:{} list:{}\n".format(hprior, l2))
+        #print("sprior:{} list:{}\n".format(sprior, l3))
         hres = calcCondProb(hprior, l2)
         sres = calcCondProb(sprior, l3)
+        #print("hres:{} sres:{}".format(hres,sres))
+        # if accurate add to counter, and total count
         if hres > sres and HorS == "HAM":
             correct+=1
+            #print("HAM, {} > {}".format(hres,sres))
         elif hres < sres and HorS == "SPAM":
             correct+=1
-    print("{}/{} correct".format(correct, total))
+            #print("SPAM, {} < {}".format(hres,sres))
+    return correct/total
+    #print("{}/{} correct, {:.2f}%".format(correct, total, (correct/total*100)))
 
 #main function
 if __name__ == "__main__":
@@ -107,15 +99,9 @@ if __name__ == "__main__":
     prior_ham = hamCount / (hamCount+spamCount)
     prior_spam = spamCount / (hamCount+spamCount)
     
-    #print("prestemmed:")
-    #l = genProbFromEmail("minitest_ham_stemmed.txt",hamBag,spamBag,"HAM");
-    #l2 = genProbFromEmail("minitest_ham_stemmed.txt",hamBag,spamBag,"SPAM");
-    #$ham chance = calcCondProb(proir_ham, l)
-    
-    #print("\nnewly stemmed:")
-    test("test/ham/",hamBag,spamBag,"HAM",prior_ham,prior_spam)
-    #test("test/ham/",hamBag,spamBag,"SPAM",prior_ham,prior_spam)
-    #test("test/spam/",hamBag,spamBag,"HAM",prior_ham,prior_spam)
-    test("test/spam/",hamBag,spamBag,"SPAM",prior_ham,prior_spam)
-    #print(calcCondProb(1,[.5,.5,.5]))
-    #print(np.exp(calcCondProb(1,[.5,.5,.5])))
+    #Naive Bayes
+    print("Running Naive Bayes with Laplace Smoothing of 1")
+    ham_res = test("test/ham/",hamBag,spamBag,"HAM",prior_ham,prior_spam)
+    spam_res = test("test/spam/",hamBag,spamBag,"SPAM",prior_ham,prior_spam)
+    print("\tDetected {:.2f}% ham correctly".format(ham_res))
+    print("\tDetected {:.2f}% spam correctly".format(spam_res))
